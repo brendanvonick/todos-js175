6cc39ea4231190d4036449b77fdd26be997cf196
brendan.vonick <brendan.vonick@moo.com>
2021-07-03 01:41:38 -0600

    adding through new list creation

---
+++ todos.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const morgan = require('morgan');
+const TodoList = require('./lib/todolist');
+
+const app = express();
+const host = 'localhost';
+const port = 3000;
+
+let todoLists = require('./lib/seed-data');
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(morgan('common'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+const compareByTitle = (todoListA, todoListB) => {
+  let titleA = todoListA.title.toLowerCase();
+  let titleB = todoListB.title.toLowerCase();
+
+  if (titleA < titleB) {
+    return -1;
+  } else if (titleA > titleB) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+const sortTodoLists = lists => {
+  let undone = lists.filter(todoList => !todoList.isDone());
+  let done   = lists.filter(todoList => todoList.isDone());
+  undone.sort(compareByTitle);
+  done.sort(compareByTitle);
+  return [].concat(undone, done);
+};
+
+app.get('/', (req, res) => {
+  res.redirect('/lists');
+});
+
+app.get('/lists', (req, res) => {
+  res.render('lists', {
+    todoLists: sortTodoLists(todoLists),
+  });
+});
+
+app.get('/lists/new', (req, res) => {
+  res.render('new-list');
+});
+
+app.post('/lists', (req, res) => {
+  let title = req.body.todoListTitle.trim();
+  todoLists.push(new TodoList(title));
+  res.redirect('/lists');
+});
+
+app.listen(port, host, () => {
+  console.log(`Todos is listening on port ${port} of ${host}!`);
+});
